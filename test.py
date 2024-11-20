9ef665f34000b117db020f76f2d6fe82f6b79829
Soham Banerjee <soham@dhcp-10-5-244-185.wireless.rochester.edu>
2024-11-20 17:02:42 -0500

    fixed some bugs

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 import numpy as np
 
-a = [2,1,3,11,4,5,7,6,23,56,114,2,4,89]
+a = np.array([2,1,3,11,4,5,7,6,23,56,114,2,4,89])
 a_ind = a.flatten().argsort()[::-1]
+a_sort = a.argsort()[::-1]
+print(a_sort)
 print(a_ind)
