f4ff706e459c6fc43de4980e2d117a34684c691d
Soham Banerjee <soham.banerjee11@gmail.com>
2024-11-21 14:46:44 -0500

    Added a file to compare a number to pi

---
+++ compare_pi.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+n = input("Enter a number-\n")
+m = int(n)
+pi = 3.14
+
+if m > pi:
+    print("Your number is greater than pi.")
+else:
+    print("Your number is smaller than or equal to pi.")
